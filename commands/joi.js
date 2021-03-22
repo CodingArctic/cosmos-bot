da86e54876d9a5afa17477af5aef211719112507
arctic <geyer.clyde@gmail.com>
2021-03-22 13:39:01 -0400

    joi wasnt named right lmao

--- commands/joi.js
+++ commands/joi.js
@@ -1,7 +1,15 @@
 module.exports = {
-	name: 'fruits',
+	name: 'joi',
 	description: 'Reacts to your message with :joi:',
 	execute(message, args) {
-		message.react(client.emojis.get("823310453333033060"));
+
+        if (message.author.id === '712492815824977942') {
+            
+            message.react('817282628377903107');
+            return;
+        }
+
+        message.reply('You\'re not joi!')
+
 	},
 };
